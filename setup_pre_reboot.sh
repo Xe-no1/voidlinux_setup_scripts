edca9a9d7d7fe484479c3bdf231511918e94eec3
Xe-no1 <mazentech10@gmail.com>
2025-04-30 15:23:48 +0300

    added python package to pre_reboot

--- setup_pre_reboot.sh
+++ setup_pre_reboot.sh
@@ -40,7 +40,7 @@ xbps-install -S grub-arm64-efi
 
 grub-install --target=arm64-efi --efi-directory=/boot/efi --bootloader-id="Void"
 
-xbps-install NetworkManager openssh terminus-font fastfetch curl tar
+xbps-install NetworkManager openssh terminus-font fastfetch curl tar python
 ln -s /etc/sv/dbus /etc/runit/runsvdir/default/
 ln -s /etc/sv/NetworkManager /etc/runit/runsvdir/default/
 ln -s /etc/sv/sshd /etc/runit/runsvdir/default/
