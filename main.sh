3b82b6ab19bb495de51af37c2832e578cf338e14
Xe-no1 <mazentech10@gmail.com>
2025-06-14 11:49:51 +0300

    testing testing

--- main.sh
+++ main.sh
@@ -2,60 +2,60 @@
 
 set -euo pipefail
 
-xbps-install -Sy gptfdisk parted
-
-echo "Warning!!! Wiping the disk in 5 seconds, press ctrl+c to interrupt."
-echo "5"
-sleep 1
-echo "4"
-sleep 1
-echo "3"
-sleep 1
-echo "2"
-sleep 1
-echo "1"
-sleep 1
-echo "Wiping now"
-
-# disk partitioning, note that `C12A7328-F81F-11D2-BA4B-00A0C93EC93B` is the GUID for an EFI system
-# `B921B045-1DF0-41C3-AF44-4C6F280D3FAE` is the GUID for a Linux ARM64 root
-# and `4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709` is the GUID for a Linux x86-64 root
-# visit 'https://en.wikipedia.org/wiki/GUID_Partition_Table#Partition_type_GUIDs' for other GUID's
-
-if mountpoint -q /mnt; then
-  umount -AR /mnt # make sure everything is unmounted before we start
-fi
-
-# using sgdisk
-sgdisk --zap-all /dev/sda                                             # zap all on disk
-sgdisk --set-alignment 2048 --clear /dev/sda                          # new gpt disk 2048 alignment
-sgdisk --new=1::+64M --typecode=1:ef00 --change-name=1:'esp' /dev/sda # partition 1 (EFI system partition)
-sgdisk --new=2::-0 --typecode=2:8305 --change-name=2:'root' /dev/sda  # partition 2 (Root partition), default start, remaining
-
-partprobe /dev/sda # reread partition table to ensure it is correct
-
-mkfs.vfat /dev/sda1
-mkfs.ext4 /dev/sda2
-
-mount /dev/sda2 /mnt
-mkdir -p /mnt/efi
-mount /dev/sda1 /mnt/efi
-
-REPO=https://repo-de.voidlinux.org/current/aarch64
-
-ARCH=aarch64
-
-mkdir -p /mnt/var/db/xbps/keys
-cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
-
-XBPS_ARCH=$ARCH xbps-install -Sy -r /mnt -R "$REPO" base-system base-devel linux-mainline linux-firmware
-
-xgenfstab -U /mnt >/mnt/etc/fstab
+# xbps-install -Sy gptfdisk parted
+#
+# echo "Warning!!! Wiping the disk in 5 seconds, press ctrl+c to interrupt."
+# echo "5"
+# sleep 1
+# echo "4"
+# sleep 1
+# echo "3"
+# sleep 1
+# echo "2"
+# sleep 1
+# echo "1"
+# sleep 1
+# echo "Wiping now"
+#
+# # disk partitioning, note that `C12A7328-F81F-11D2-BA4B-00A0C93EC93B` is the GUID for an EFI system
+# # `B921B045-1DF0-41C3-AF44-4C6F280D3FAE` is the GUID for a Linux ARM64 root
+# # and `4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709` is the GUID for a Linux x86-64 root
+# # visit 'https://en.wikipedia.org/wiki/GUID_Partition_Table#Partition_type_GUIDs' for other GUID's
+#
+# if mountpoint -q /mnt; then
+#   umount -AR /mnt # make sure everything is unmounted before we start
+# fi
+#
+# # using sgdisk
+# sgdisk --zap-all /dev/sda                                             # zap all on disk
+# sgdisk --set-alignment 2048 --clear /dev/sda                          # new gpt disk 2048 alignment
+# sgdisk --new=1::+64M --typecode=1:ef00 --change-name=1:'esp' /dev/sda # partition 1 (EFI system partition)
+# sgdisk --new=2::-0 --typecode=2:8305 --change-name=2:'root' /dev/sda  # partition 2 (Root partition), default start, remaining
+#
+# partprobe /dev/sda # reread partition table to ensure it is correct
+#
+# mkfs.vfat /dev/sda1
+# mkfs.ext4 /dev/sda2
+#
+# mount /dev/sda2 /mnt
+# mkdir -p /mnt/efi
+# mount /dev/sda1 /mnt/efi
+#
+# REPO=https://repo-de.voidlinux.org/current/aarch64
+#
+# ARCH=aarch64
+#
+# mkdir -p /mnt/var/db/xbps/keys
+# cp /var/db/xbps/keys/* /mnt/var/db/xbps/keys/
+#
+# XBPS_ARCH=$ARCH xbps-install -Sy -r /mnt -R "$REPO" base-system base-devel linux-mainline linux-firmware
+#
+# xgenfstab -U /mnt >/mnt/etc/fstab
 
 ###############################
 
-username="mazentech"
-answer="yes"
+export username="mazentech"
+export answer="yes"
 
 # cat <<EOC >/mnt/chrootcmds
 
@@ -128,7 +128,7 @@ EOF
 
 export -f install_func
 
-xchroot /mnt /bin/bash -c "install_func"
+xchroot /mnt install_func
 
 if mountpoint -q /mnt; then
   umount -AR /mnt
